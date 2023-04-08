be0ccae07bbd96da740e00a189c04e2cefb43455
joshbrusa <joshbrusa@gmail.com>
2023-04-08 14:49:26 -0700

    fix dockerfiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -65,8 +65,6 @@ export default function App() {
   const [authenticating, setAuthenticating] = useState(true);
   const [user, setUser] = useState<User>(null);
 
-  console.log(import.meta.env.VITE_BACKEND_ORIGIN);
-
   useEffect(() => {
     async function check() {
       try {
